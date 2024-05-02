bdef758e409404d72df9c6524059943819296e83
Mazafesi0 <jaimemarcu@gmail.com>
2024-05-02 17:14:13 +0200

    Update

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "http://localhost"
+    "*"
   );
   res.setHeader(
     "Access-Control-Allow-Methods",
